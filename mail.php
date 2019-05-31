f48b5da80d95a770736ec3f66d0d397a3da43c64
Puneeth1996 <38997799+Puneeth1996@users.noreply.github.com>
2019-05-31 02:25:20 -0700

    Puneeth :Contact Section With PHP Server Side Code
    Have Implemented the contact page and some PHP server side code for the same

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+    $to = 'puneeth1996trainer@gmail.com,puneethskingston@gmail.com,puneeth1996p@gmail.com,happiplaykids@gmail.com';
+    $firstname = $_POST["name"];
+    $email= $_POST["email"];
+    $text= $_POST["message"];
+    $phone= $_POST["phone"];
+    
+
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message =
+    '<table style="width:100%">
+        <tr>
+            <td>'.$firstname.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>phone: '.$phone.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+        
+    </table>';
+
+    if (@mail($to, $email, $message, $headers))
+    {
+        header('location: happyplaykids.html');
+    }else{
+        echo 'Failed';
+    }
+
+?>
--- readme.md
+++ readme.md
@@ -8,3 +8,26 @@ Have made the contents of nav bar properly meanigful <br>
 9.30pm
 
 Have completed with the navigation functionality.
+
+May 31 2 pm
+
+I have fixed on some missing closing div tags that were not existing in the project folder so far.
+
+Have added a simple bootstrap form from codepen article 
+https://codepen.io/SeanWhelan/pen/avMWqQ
+
+have added some bootstrap based code for creating on the form for contact us 
+
+Should start setting up some PHP server side code 
+
+https://1stwebdesigner.com/php-contact-form-html/
+
+Trying to write better server side code using 
+https://colorlib.com/wp/template/eclipse/
+
+Have succesfully implemented the contact page,
+
+Just some finishing touched on the same with the Happy Play Kids Info for all the lorem ipsum would solve the issues
+
+
+
